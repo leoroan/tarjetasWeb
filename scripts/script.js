c54d9f032ba2a94855a5be5891e87ec2d0a95322
Leandro <leoroan@gmail.com>
2023-05-09 22:26:17 -0300

    basic form and template done

--- scripts/script.js
+++ scripts/script.js
@@ -1,47 +1,19 @@
-console.log("this form script is working!");
+console.log("credits:");
+console.log("https://github.com/levinunnink/html-form-to-google-sheet");
 
 
-function postToGoogle() {
-  var field1 = $("#asistencia").val();
-  var field2 = $("#nombre").val();
-  var field3 = $("#apellido").val();
-
-  $.ajax({
-    url: "https://docs.google.com/forms/u/0/d/e/1FAIpQLSeuk1op-y0oX3Y5GafElJCZBMqX2Btct8Wg7Bs2MaSYomptFA/formResponse",
-
-    //add your google form generated numbers below which are also the 'names' of your inputs     
-    data: {
-      "entry.1607674031": field1,
-      "entry.1898245126": field2,
-      "entry.1212490641": field3
-    },
-    type: "POST",
-    dataType: "xml",
-    success: function (d) {
-      $('#contact').trigger('reset');
-    },
-    error: function (x, y, z) {
-      $('#contact').trigger('reset');
-    }
+window.addEventListener("load", function() {
+  const form = document.getElementById('contacto');
+  form.addEventListener("submit", function(e) {
+    e.preventDefault();
+    const data = new FormData(form);
+    const action = e.target.action;
+    fetch(action, {
+      method: 'POST',
+      body: data,
+    })
+    .then(() => {
+      alert("Te registraste correctamente! nos vemos en el evento");
+    })
   });
-  return false;
-}
-
-// function postToGoogle() {
-//   var field1 = $("#asistencia").val();
-//   var field2 = $("#nombre").val();
-//   var field3 = $("#apellido").val();
-
-//   $.getJSON("https://docs.google.com/forms/u/0/d/e/1FAIpQLSeuk1op-y0oX3Y5GafElJCZBMqX2Btct8Wg7Bs2MaSYomptFA/formResponse?callback=?", {
-//     //add your google form generated numbers below which are also the 'names' of your inputs     
-//     "entry.1607674031": field1,
-//     "entry.1898245126": field2,
-//     "entry.1212490641": field3
-//   }).done(function (response) {
-//     $('#contact').trigger('reset');
-//   }).fail(function () {
-//     $('#contact').trigger('reset');
-//   });
-
-//   return false;
-// }
+});
